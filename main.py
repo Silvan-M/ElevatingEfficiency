63eac461a1a26bf1ffa9a00e075fef3237c8e482
Silvan-M <xbotraid@gmail.com>
2023-11-26 19:12:23 +0100

    Changes

--- main.py
+++ main.py
@@ -14,8 +14,6 @@ simulation = Simulation(
     Building(
             elevators = [
                 Elevator(0, floorAmount-1, FCFSPolicy(), 0, 10),
-                Elevator(0, floorAmount-1, LOOKPolicy(), 0, 10),
-                Elevator(0, floorAmount-1, LOOKPolicy(), 1, 10),
             ],
             floorAmount = floorAmount,
             spawnDistribution = Distribution(floorAmount, DistrType.UNIFORM),
