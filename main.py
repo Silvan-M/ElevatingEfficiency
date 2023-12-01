7be1030a2b8991fcca48d8f03e93dc3d8999dd6e
Silvan-M <xbotraid@gmail.com>
2023-12-01 13:22:02 +0100

    Update simulation run time in main.py

--- main.py
+++ main.py
@@ -89,11 +89,7 @@ if (showGui):
 if (showLivePlot):
     livePlot = LivePlotter(simulation, [Objective.AWT, Objective.AWTSD, Objective.ACE])
 
-<<<<<<< HEAD
-simulation.run(seconds=5000, timeScale=-1)
-=======
 simulation.run(hours=1, timeScale=-1)
->>>>>>> e8aca7b84f8f8747d434070fc25df2ee9892a470
 
 
 if (DB.mnEnd):
