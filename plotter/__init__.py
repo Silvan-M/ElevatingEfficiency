bd5a3b13f97472b9e599d56dc2f1ec86cd4cd47c
Silvan-M <xbotraid@gmail.com>
2023-12-17 17:53:02 +0100

    Update plotter module and add plotter2D and plotter3D classes

---
+++ plotter/__init__.py
@@ -0,0 +1,8 @@
+# This is the __init__ file for the policies package.
+# It is used to import all policies to the main file.
+#
+# Usage: from plotter import {PlotterName}
+
+
+from .plotter2D import Plotter2D
+from .plotter3D import Plotter3D
